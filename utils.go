b2286304da698fc6cecff05ad5e3930cde0356fa
Filipp Demchenko <filipp5632@gmail.com>
2023-12-24 14:06:23 +0200

    Implement basic brightness control CLI

---
+++ utils.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getNumberFromFile(filename string) (int, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return 0, nil
+	}
+
+	value, err := strconv.Atoi(strings.TrimSpace(string(content)))
+	if err != nil {
+		return 0, nil
+	}
+	return value, nil
+}
+
+func checkBounds(value, minValue, maxValue int) int {
+	if value < minValue {
+		return minValue
+	}
+	if value > maxValue {
+		return maxValue
+	}
+	return value
+}
